dcda07aaf52b98cec0ab2d5f27b0b06d5c5df913
Erik Daniel <danielerik949@gmail.com>
2025-06-29 23:24:08 -0300

    logica del login

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Register
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registerForm");
+
+  // Validar que el formulario exista
+  if (!form) return;
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -33,6 +38,45 @@ document.addEventListener("DOMContentLoaded", () => {
       } else {
         const error = await response.json();
         alert("Error al registrar: " + (error.message || "Intente nuevamente."));
+      }
+    } catch (err) {
+      console.error("Error al enviar la solicitud:", err);
+      alert("Ocurrió un error. Intente más tarde.");
+    }
+  });
+});
+
+
+// Login
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+
+  // Validar que el formulario exista
+  if (!form) return;
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = form.elements["email"].value.trim();
+    const password = form.elements["password"].value;
+
+    const data = { email, password };
+
+    try {
+      const response = await fetch("https://node-production-d05b.up.railway.app/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        const result = await response.json();
+        alert("Inicio de sesión exitoso. Redirigiendo al usuario a la página principal.");
+        window.location.href = "index.html";
+      } else {
+        // Leer el mensaje de error del backend
+        const error = await response.json();
+        alert("Error al iniciar sesión: " + (error.error || "Intente nuevamente."));
       }
     } catch (err) {
       console.error("Error al enviar la solicitud:", err);
