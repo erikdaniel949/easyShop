5d74bbda87126803ef4c741da45629a37c39832c
erikdaniel949 <danielerik949@gmail.com>
2025-07-05 21:24:42 -0300

    link del nuevo backend

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = { name, email, password };
 
     try {
-      const response = await fetch("https://node-production-d05b.up.railway.app/register", {
+      const response = await fetch("https://backend-production-4cbf.up.railway.app/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = { email, password };
 
     try {
-      const response = await fetch("https://node-production-d05b.up.railway.app/login", {
+      const response = await fetch("https://backend-production-4cbf.up.railway.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const showDatabase = async () => {
   try {
-    const response = await fetch("https://node-production-d05b.up.railway.app/showDatabase");
+    const response = await fetch("https://backend-production-4cbf.up.railway.app/showDatabase");
     if (response.ok) {
       const data = await response.json();
       const fragment = document.createDocumentFragment();
