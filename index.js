6b4109ea66f00b84f13dce30db1c778212d7e39e
erikdaniel949 <danielerik949@gmail.com>
2025-08-24 23:56:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-function mostrarDetalle(nombre, precio, descripcion) {
+// funcion para verificar si el usuario está logueado
+    const checkSession = async () => {
+      const response = await fetch('http://localhost:3000/isLogged', {
+        method: 'GET',
+        credentials: 'include' // Asegura que se envíen las cookies
+      })
+      if(response.ok) {
+        const data = await response.json();
+        const sessionBtn = document.getElementById('sessionBtn');
+        sessionBtn.textContent = 'Mi cuenta';
+        sessionBtn.href = 'profile.html';
+        
+        
+      } else {
+        console.error('Error al verificar la sesión');
+      }
+    }
+    checkSession();
+    
+
+
+    function mostrarDetalle(nombre, precio, descripcion) {
   document.getElementById("detalle-nombre").textContent = nombre;
   document.getElementById("detalle-precio").textContent = "Precio: " + precio;
   document.getElementById("detalle-descripcion").textContent = descripcion;
