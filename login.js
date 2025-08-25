38b94d636ee58e5f75da109e501f8b4d09b7d61c
erikdaniel949 <danielerik949@gmail.com>
2025-08-25 00:24:32 -0300

    Update login.js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", async (e) => {
   const data = { email, password };
 
   try {
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://back-es-yjar.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
