adc48a85f4aab7321ccda6a826ffdb672c5d3b3d
erikdaniel949 <danielerik949@gmail.com>
2025-08-29 00:30:04 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,3 @@
-const form = document.getElementById("loginForm");
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const email = form.elements["email"].value.trim();
@@ -8,7 +6,7 @@ form.addEventListener("submit", async (e) => {
   const data = { email, password };
 
   try {
-    const response = await fetch("https://back-es-yjar.onrender.com/login", {
+    const response = await fetch("http://localhost:3000/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,14 +14,17 @@ form.addEventListener("submit", async (e) => {
       body: JSON.stringify(data),
       credentials: "include" // Incluir cookies en la solicitud
     });
-    if (response.ok) {
-      const result = await response.json();
-      window.location.href = "index.html";
-    } else {
+
+    if (!response.ok) {
       // Leer el mensaje de error del backend
       const result = await response.json();
       alert("Error al iniciar sesión: " + (result.error || "Intente nuevamente."));
+      return; // <-- detener aquí para no redirigir
     }
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 500);
+
   } catch (err) {
     console.error("Error al enviar la solicitud:", err);
     alert("Ocurrió un error. Intente más tarde.");
